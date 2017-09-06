da99fdf7693336de4509638d1ace0b8a04b6d0fe
liuxueyang <zypz457@sina.com>
2017-09-06 20:58:15 +0800

    add src/main.cpp
    create a sample source code file

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  for (i = 0; i < N; ++i) {
+    // TODO
+  }
+
+  while (true) {
+    // TODO
+  }
+
+  return 0;
+}
