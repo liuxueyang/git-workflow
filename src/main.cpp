4ef5d8ab3584c79952cf7b311e5b4177884e9b7c
liuxueyang <zypz457@sina.com>
2017-09-06 21:41:23 +0800

    add statements to for/while on branch hw-rebase-conflict
    1. remove two TODOs
    2. cout some message in for and while loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+  int i, N = 2333;
+
   for (i = 0; i < N; ++i) {
-    // TODO
+    // change (1) made on branch hw-rebase-conflict
+    cout << i * i << "\n";
   }
 
   while (true) {
-    // TODO
+    // change (2) made on branch hw-rebase-conflict
+    // smile
+    cout << ":)))" << "\n";
   }
 
   return 0;
