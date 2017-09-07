739b839796c60374df203fdc2fcff6730d7eb34c
liuxueyang <zypz457@sina.com>
2017-09-07 21:27:48 +0800

    add some comment in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,23 @@
 
 using namespace std;
 
+// define some macro
+
 int main(int argc, char *argv[])
 {
+  // and so on
+
   for (i = 0; i < N; ++i) {
     // TODO
   }
 
+  // another modification
+
   while (true) {
     // TODO
   }
+
+  // some modification
 
   return 0;
 }
